dd77604f09a4af4df9a6c4c620f3556e2bc78238
Ansh Goyal <94157520+anshgoyalevil@users.noreply.github.com>
2021-12-28 07:49:02 +0530

    Update birthday-cake-candles.java

--- birthday-cake-candles.java
+++ birthday-cake-candles.java
@@ -14,7 +14,6 @@ class Result {
 
     /*
      * Complete the 'birthdayCakeCandles' function below.
-     *
      * The function is expected to return an INTEGER.
      * The function accepts INTEGER_ARRAY candles as parameter.
      */
